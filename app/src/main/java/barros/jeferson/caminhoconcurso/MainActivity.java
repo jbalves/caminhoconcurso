473fe156b58347f0cc805a73dd1125afd1703ca0
Jeferson Barros <im.jbalves@gmail.com>
2016-10-17 21:35:24 -0400

    Initial setup for JSONObject

--- app/src/main/java/barros/jeferson/caminhoconcurso/MainActivity.java
+++ app/src/main/java/barros/jeferson/caminhoconcurso/MainActivity.java
@@ -3,11 +3,18 @@ package barros.jeferson.caminhoconcurso;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.oceanbrasil.libocean.control.http.Request;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ObjectStreamException;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Request.RequestListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +46,37 @@ public class MainActivity extends AppCompatActivity {
     public void openObjetivos(View view) {
         Intent intent = new Intent(this, ObjetivosAcitivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onRequestOk(String resposta, JSONObject jsonObject, int code) {
+        if (code == Request.NENHUM_ERROR) {
+            Log.d("Debug","resposta: " + resposta);
+
+            if (resposta !=null) {
+                try {
+
+                    JSONObject object = new JSONObject(resposta);
+
+                    JSONArray concursos = object.getJSONArray("concursos");
+
+                    for (int i = 0; i < concursos.length(); i++) {
+
+                        JSONObject concurso = concursos.getJSONObject(i);
+                        String orgao = concurso.getString("orgao");
+                        String estado = concurso.getString("estado");
+                        String ano = concurso.getString("ano");
+                        String banca = concurso.getString("banca");
+
+                        JSONArray livros = item.getJSONArray("livros");
+                        for (int j=0; j<livros.length();j++){
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
     }
 }
